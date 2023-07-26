50e7132101928852e0b3fe92fcf8f09c4374b773
RushabhNagpure <114604410+RushabhNagpure@users.noreply.github.com>
2023-07-26 12:37:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,20 +4,13 @@ const person = {
   name: "John Doe",
   age: 80,
 }
- const team = {
-window.players = players;
-window.person = person;
-window.team = team;
-window.team1 = team1;
-window.cap1 = cap1;
-	 }
-	
-const team1 = ["John", "Bob", "Alice", "Poppy"];
+ let team = players;
 
-const cap1 = {
-  name: "John Doe",
-  age: 80,
-}
+// Create a variable named 'team1' that is a copy of the 'players' array
+let team1 = [...players];
+
+// Create a variable named 'cap1' that is a copy of the 'person' object
+let cap1 = {...person};
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
 window.players = players;
